7146c47b28d1c45e6f7aca3e3c9d7db93340c683
ama28 <amm28y@gmail.com>
2022-12-01 14:51:35 -0500

    modal styles

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,12 +4,11 @@ import '../App.css';
 const MODAL_STYLE = {
   position: "absolute",
   backgroundColor: "#FFF",
-  color: 'gray',
-  lineHeight: '4em',
-  padding: "100px 160px",
+  display: 'flex',
+  flexDirection: 'column',
   zIndex: "1000",
-  width: "55vw",
-  top: "15%",
+  width: "70vw",
+  top: "10%",
   borderRadius: "10px",
 };
 const OVERLAY_STYLE = {
@@ -26,8 +25,8 @@ const OVERLAY_STYLE = {
 };
 
 const SPACER_STYLE = {
-  height: '200px',
-  margin: '0 0 -200px 0',
+  height: '100px',
+  margin: '0 0 -100px 0',
 }
 
 const handleChildElementClick = (e) => {
@@ -41,17 +40,18 @@ export default function Modal(props) {
     <>
       <div style={OVERLAY_STYLE} onClick={() => props.closeModal()}>
         <div style={MODAL_STYLE} onClick={(e) => handleChildElementClick(e)}>
-          <img src={process.env.PUBLIC_URL + "/assets/images/minecraft.jpg"} width="100%"></img>
-          <button onClick={() => props.closeModal()}>Close Modal</button>
-
+          <img src={process.env.PUBLIC_URL + "/assets/images/minecraft.jpg"} className="modalHeader"></img>
+          {/* <button onClick={() => props.closeModal()}>Close Modal</button> */}
+          <div className="modalContent">
+          <h1>Minecraft</h1>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet efficitur erat. Praesent eu mollis erat, et pretium quam. Nam laoreet iaculis velit nec placerat. Aliquam ut magna accumsan, porta sem eget, maximus magna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed dui nisl, maximus ac convallis eget, interdum sed urna. Etiam pellentesque at dolor vitae venenatis. Suspendisse vitae ullamcorper mauris. Nulla facilisis turpis vel metus varius ultricies. Curabitur id interdum augue, et lobortis ante.
-
-          Suspendisse laoreet, diam sed tincidunt porta, sem tellus ultrices lectus, in auctor mauris nisi at massa. Pellentesque vitae dapibus erat. In hac habitasse platea dictumst. Mauris bibendum orci in urna elementum, ac mattis erat sagittis. Aenean suscipit nisi lacus. Pellentesque sagittis est arcu, suscipit interdum purus ultricies sit amet. Suspendisse eleifend iaculis odio, eu consectetur erat elementum ut. Pellentesque imperdiet purus eget sapien vulputate, non gravida odio vulputate. Sed rutrum feugiat risus, non vehicula est hendrerit vitae. Suspendisse consectetur velit in ipsum posuere, varius sagittis tellus dignissim. Mauris nec dapibus odio. Maecenas sit amet arcu erat. Maecenas consectetur et neque quis semper. Suspendisse fringilla ex eget odio malesuada iaculis. Proin eu augue vel neque efficitur dignissim eget quis nulla.
+          <h3>The Art Direction</h3>
+          Suspendisse laoreet, diam sed tincidunt porta, sem tellus ultrices lectus, in auctor mauris nisi at massa. Pellentesque vitae dapibus erat. In hac habitasse platea dictumst. Mauris bibendum orci in urna elementum, ac mattis erat sagittis. Aenean suscipit nisi lacus. <img src={process.env.PUBLIC_URL + "/assets/images/annogameplay.jpeg"}></img>Pellentesque sagittis est arcu, suscipit interdum purus ultricies sit amet. Suspendisse eleifend iaculis odio, eu consectetur erat elementum ut. Pellentesque imperdiet purus eget sapien vulputate, non gravida odio vulputate. Sed rutrum feugiat risus, non vehicula est hendrerit vitae. <br></br>Suspendisse consectetur velit in ipsum posuere, varius sagittis tellus dignissim. Mauris nec dapibus odio. Maecenas sit amet arcu erat. Maecenas consectetur et neque quis semper. Suspendisse fringilla ex eget odio malesuada iaculis. Proin eu augue vel neque efficitur dignissim eget quis nulla.
 
           Maecenas id diam porta, vulputate eros non, sodales metus. Pellentesque vel nisi non erat vestibulum eleifend. Phasellus lacinia arcu ut neque pulvinar auctor. Curabitur tristique malesuada mauris eu laoreet. Mauris tincidunt risus id pulvinar eleifend. In elementum dolor nec tellus imperdiet, vitae maximus massa tristique. Fusce ac metus vel purus fringilla vestibulum. Nam laoreet dolor felis, in viverra velit elementum non. Quisque iaculis neque vitae ultrices dictum. Nunc sed lectus vulputate, vulputate sem eu, finibus lorem. Nullam non augue viverra, ultricies leo vel, tincidunt tellus.
 
           Phasellus ultrices ex a arcu consequat fringilla. Nullam purus neque, porttitor a elementum ut, condimentum sit amet dolor. Sed vitae fermentum mauris. Suspendisse suscipit ut metus eu mollis. Morbi non enim facilisis, elementum justo vel, mattis felis. Nulla id varius est. Suspendisse potenti. Nam in vehicula diam. Vivamus dignissim purus vitae tempor egestas. Ut non dictum purus.
-
+          </div>
           <div style={SPACER_STYLE}></div>
         </div>
       </div>
